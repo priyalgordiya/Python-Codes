ce15f0f7d968c98d134afce0f0d303ad84e8dab3
Priyal Gordiya <142414167+priyalgordiya@users.noreply.github.com>
2023-08-19 19:27:43 +0530

    Add files via upload

---
+++ CUBOID.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 19 18:49:55 2023
+
+@author: priya
+"""
+
+import turtle
+t=turtle.Turtle()
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(180)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(60)
+t.forward(50)
+t.left(30)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(180)
+t.forward(100)
+t.left(60)
+t.forward(50)
+t.right(180)
+t.forward(50)
+t.left(30)
+t.forward(100)
+t.left(150)
+t.forward(50)
+t.right(180)
+t.forward(50)
+t.right(60)
+t.forward(100)
+t.right(120)
+t.forward(50)
+t.hideturtle()
+
+
+
+
