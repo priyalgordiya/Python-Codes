68b32df43a6e2848e12c420fba20a2ca7dff6fcb
Priyal Gordiya <142414167+priyalgordiya@users.noreply.github.com>
2023-08-19 18:02:03 +0530

    Add files via upload

---
+++ FAN.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 19 17:36:31 2023
+
+@author: priya
+"""
+
+import turtle
+t=turtle.Turtle()
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(180)
+t.forward(100)
+t.right(160)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(70)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.hideturtle()
---
+++ SIERPINSKI TREE.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 19 17:48:24 2023
+
+@author: priya
+"""
+
+import turtle
+t=turtle.Turtle()
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(60)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.hideturtle()
+
+
+
