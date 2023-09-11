2271c75c97703c2e3b0e723c0bb7e6c35bf58610
Priyal Gordiya <142414167+priyalgordiya@users.noreply.github.com>
2023-09-11 18:02:21 +0530

    Add files via upload

---
+++ CONE SMILING FACE.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 22 15:59:11 2023
+
+@author: priya
+"""
+
+import turtle
+t=turtle.Turtle()
+t.forward(200)
+t.left(120)
+t.forward(200)
+t.left(120)
+t.forward(200)
+t.left(120)
+t.right(90)
+t.circle(100, 180)
+t.penup()
+t.goto(50, -50)
+t.pendown()
+t.right(90)
+t.circle(17)
+t.penup()
+t.goto(150, - 50)
+t.pendown()
+t.circle(17)
+t.penup()
+t.goto(78, -60)
+t.pendown()
+t.right(90)
+t.circle(10, 180)
+t.right(180)
+t.circle(10, 180)
+t.penup()
+t.goto(88, -70)
+t.pendown()
+t.left(180)
+t.circle(10, 180)
+t.penup()
+t.goto(0, 0)
+t.pendown(0)
+
+
+
+
+
+
