c963ecb3c1afff974dd80dd1489e37bbaccca87e
Priyal Gordiya <142414167+priyalgordiya@users.noreply.github.com>
2023-10-09 13:29:57 +0530

    Add files via upload

---
+++ Increasing & decreasing asterik triangle.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 13:12:08 2023
+
+@author: priya
+"""
+
+a = ''
+for i in range(5):
+    a = a+'*'
+    print(a)
+a = '****'
+for i in range(4):
+        print(a)
+        a = a [:-1]
